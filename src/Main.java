9a40c14d1e725830817d1f6325e85c1d7a67cd21
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 13:40:20 +0100

    Arithmetic operators

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,40 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // first declare all the variables
+        // Arithmetic Operators
+//
+//        int x = 10;
+//        int y = 3;
+//        int z ;
+//        z = x + y;
+//        z  = x - y;
+//        z = x * y;
+//        z = x / y;
+//        z = x % y;
+         // Augmented Assignment Operators
+//        x = x + y;
+//        x+=y;
+//        x = x - y ;
+//        x-=y;
+//        x=x*y;
+//        x*=y;
+//        x = x / y ;
+//        x/=y;
+//        x = x% y;
+//        x %=y;
+        // Increment and Decrement operators
+//        int x = 1;
+//
+//         x++;
+//         x--;
+        // Order of operators
+        double result = 3 + 4 * (7-5) / 2.0;
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        System.out.println(result);
 
-        System.out.print("Enter an adjective (description): ");
-        adjective1 = scanner.nextLine();
-        System.out.print("Enter a noun (animal or person): ");
-        noun1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description):  ");
-        adjective2 = scanner.nextLine();
-        System.out.print("Enter a verb end with -ing (action): ");
-        verb1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description): ");
-        adjective3 = scanner.nextLine();
 
-        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
-        System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was " + adjective3 + "!");
 
-        scanner.close();
+
+
     }
 }
