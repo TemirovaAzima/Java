329b5a097f25e4ab267deb40d74b64d9f47cc923
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 12:57:27 +0100

    Calculate area of a rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,27 @@
-import java.sql.SQLOutput;
 import java.util.Scanner ;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Calculate area of a rectangle
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
         Scanner scanner = new Scanner(System.in);
-                // scanner the name of the object
-        // our scanner object can read the user input
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine(); // the difference between netxLine() and
-        // next() is next does read till a space occur, but nexline is opposite
-        // will make able to type name of users
 
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
 
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
 
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
+        area = width * height;
+
+        System.out.println("The area is: " + area + "cm²");
 
 
-        System.out.println("Hello " + name );
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your gpa is: " + gpa );
-//        System.out.println("Student: " + isStudent);
-
-        if(isStudent){
-            System.out.println("You are enrolled as a student");
-        }
-        else{
-            System.out.println("You are NOT enrolled");
-        }
-
-        // Common issues
-        System.out.print("Enter your age: ");
-        int age2 = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.print("Enter your favorite color");
-        String color = scanner.nextLine();
-
-        System.out.println("You are " + age2 + " years old");
-        System.out.println("You like the color " + color);
-
-
-
-        scanner.close();
+         scanner.close();
         // if u dont close it can lead to the unexpected behaviour
     }
 }
