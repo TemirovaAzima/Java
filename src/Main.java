069f89bccc3ce8d67bb56d7fffac9bad56a27cfa
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 13:23:52 +0100

    Mad Libs game

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
-import java.util.Scanner ;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Calculate area of a rectangle
-        double width = 0;
-        double height = 0;
-        double area = 0;
-
         Scanner scanner = new Scanner(System.in);
+        // first declare all the variables
 
-        System.out.print("Enter the width: ");
-        width = scanner.nextDouble();
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
-        System.out.print("Enter the height: ");
-        height = scanner.nextDouble();
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description):  ");
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb end with -ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
 
-        area = width * height;
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
 
-        System.out.println("The area is: " + area + "cm²");
-
-
-         scanner.close();
-        // if u dont close it can lead to the unexpected behaviour
+        scanner.close();
     }
 }
