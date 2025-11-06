ed6b51ee859f1af3da47b383dd3a298ecbe93464
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 16:57:47 +0100

    Ternary operator

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,24 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-     // Weight Conversion Program
+     // ternary operator
 
-        // Declare variables
+//        int score = 78;
+//
+//        String passOrFail =  (score>=60 ) ?  "pass" : "fail";
+//        System.out.println(passOrFail);
 
-        Scanner scanner = new Scanner(System.in);
+//        int number = 72;
+//        String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+//        System.out.println(evenOrOdd);
 
-        double weight ;
-        double newWeight;
-        int choice;
+//        int hours = 20;
+//
+//        String timeOfDay = (hours>=12) ? "P.M" : "A.M";
+//        System.out.println(timeOfDay);
+        int income = 30000;
 
-        System.out.println("Weight conversion program");
-        System.out.println("1: Convert lbs to kgs ");
-        System.out.println("2: Convert kgs to lbs ");
-
-        System.out.print("Choose an option: ");
-        choice = scanner.nextInt();
-
-        if (choice == 1){
-            System.out.print("Enter the weight in lbs: ");
-            weight = scanner.nextDouble();
-            newWeight = weight * 0.453592;
-            System.out.println("The new weight in kgs is: " + newWeight);
-        }
-        else if (choice == 2){
-            System.out.print("Enter the weight in kgs: ");
-            weight = scanner.nextDouble();
-            newWeight = weight * 2.20462 ;
-            System.out.println("The new weight in lbs is: " + newWeight);
-        }
-        else {
-            System.out.println("That was not a valid choice ");
-        }
-
-        scanner.close();
-        // welcome message
-
-        // prompt for user choice
-
-        // option 1 convert lbs to kgs
-
-        // option 2 convert kgs to bs
-
-        // else print not a valid choice
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+        System.out.println(taxRate);
     }
 }
