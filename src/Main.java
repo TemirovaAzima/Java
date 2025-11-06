c6658625ff39654cf2c7735dc3640f356e157ce3
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 16:07:11 +0100

    String Methods

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,27 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println(Math.PI );
-//        double result;
-//        result = Math.pow(2,5);
-//        result = Math.abs(-5);
-//        result = Math.sqrt(9);
-//        result = Math.round(3.74);
-//        result = Math.ceil(3.14);
-//        result = Math.floor(3.14);
-//        result = Math.max(10,20);
-//        result = Math.min(10,20);
-//        System.out.println(result);
-        // Hypotenuse c = math.sqrt(a² + b²)
-      Scanner scanner = new Scanner(System.in);
-//       double a;
-//       double b;
-//       double c;
-//
-//        System.out.print("Enter the length of side A: ");
-//        a = scanner.nextDouble();
-//
-//        System.out.print("Enter the length of side B: ");
-//        b = scanner.nextDouble();
-//
-//        c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-//        System.out.println("The hypotenuse (side c) is: " + c + "cm");
+        String name = "Password";
+
+//        int length = name.length();
+//        char letter = name.charAt(0);
+//        int index = name.indexOf(" ");
+//        int lastIndex = name.lastIndexOf("a");
+
+//        name = name.toUpperCase();
+//        name = name.toLowerCase();
+//        name = name.trim();
+//        name = name.replace("a","o");
 
 
-        // circumference = 2 * Math.PI * radius;
-        // area = Math.PI * Math.pow(radius,2);
-        // volume = (4.0 / 3.0) * Math.PI * Math.pow(radius,3)
-
-        double radius;
-        double circumference;
-        double area;
-
-        System.out.print("Enter the radius: ");
-        radius = scanner.nextDouble();
-
-        circumference = 2 * Math.PI * radius ;
-        area = Math.PI * Math.pow(radius,2);
-        System.out.println("The circumference is: " + circumference + "cm");
-        System.out.println("The area is: " + area + "cm²");
-        scanner.close();
+//        System.out.println(name.isEmpty());
+        if(name.equalsIgnoreCase("password")){
+            System.out.println("Your name cannot be password");
+        }
+        else{
+            System.out.println("Hello " + name);
+        }
 
 
     }
