dba44de9777fbaf8afd55dea53b51283381edba0
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 16:41:53 +0100

    Weight Conversion Program

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,49 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+     // Weight Conversion Program
 
-        String name = "Password";
+        // Declare variables
 
-//        int length = name.length();
-//        char letter = name.charAt(0);
-//        int index = name.indexOf(" ");
-//        int lastIndex = name.lastIndexOf("a");
+        Scanner scanner = new Scanner(System.in);
 
-//        name = name.toUpperCase();
-//        name = name.toLowerCase();
-//        name = name.trim();
-//        name = name.replace("a","o");
+        double weight ;
+        double newWeight;
+        int choice;
 
+        System.out.println("Weight conversion program");
+        System.out.println("1: Convert lbs to kgs ");
+        System.out.println("2: Convert kgs to lbs ");
 
-//        System.out.println(name.isEmpty());
-        if(name.equalsIgnoreCase("password")){
-            System.out.println("Your name cannot be password");
+        System.out.print("Choose an option: ");
+        choice = scanner.nextInt();
+
+        if (choice == 1){
+            System.out.print("Enter the weight in lbs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 0.453592;
+            System.out.println("The new weight in kgs is: " + newWeight);
         }
-        else{
-            System.out.println("Hello " + name);
+        else if (choice == 2){
+            System.out.print("Enter the weight in kgs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.20462 ;
+            System.out.println("The new weight in lbs is: " + newWeight);
+        }
+        else {
+            System.out.println("That was not a valid choice ");
         }
 
+        scanner.close();
+        // welcome message
 
+        // prompt for user choice
+
+        // option 1 convert lbs to kgs
+
+        // option 2 convert kgs to bs
+
+        // else print not a valid choice
     }
 }
