e8d9c5c43c04653d593c0cb644774b71441b582e
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-06 12:45:12 +0100

    Class Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,53 @@
+import java.sql.SQLOutput;
+import java.util.Scanner ;
+
 public class Main {
     public static void main(String[] args) {
-        // this is my first java program
-        /*
-          This
-          is
-          a milt-line
-          comment
-         */
-          //        int age ;  // declaration
-          int age = 21;
-          //        int age = 30.5; // it is a double not int
-          int year = 2025;
-          int quantity = 1;
 
-          // Primitive = simple value stored directly in memory(stack)
-          // Reference = memory address (stack) that points to the (heap)
+        Scanner scanner = new Scanner(System.in);
+                // scanner the name of the object
+        // our scanner object can read the user input
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine(); // the difference between netxLine() and
+        // next() is next does read till a space occur, but nexline is opposite
+        // will make able to type name of users
 
-          // Primitive vs Reference
-          // int           string
-          // double        array
-          // char          object
-          // boolean
-          double price = 19;
-          double gpa = 3.5;
-          double temperature = -12.5;
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
 
-          char grade = 'A'; // single quotes
-          char symbol = '!';
-          char currency = '$';
+        System.out.print("What is your gpa: ");
+        double gpa = scanner.nextDouble();
+
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
 
 
-          boolean isStudent = true;
-          boolean isOnline = true;
-          boolean forSale = true;
+        System.out.println("Hello " + name );
+        System.out.println("You are " + age + " years old");
+        System.out.println("Your gpa is: " + gpa );
+//        System.out.println("Student: " + isStudent);
 
-          if(isStudent){
-            System.out.println("You are a student");
-         }
+        if(isStudent){
+            System.out.println("You are enrolled as a student");
+        }
+        else{
+            System.out.println("You are NOT enrolled");
+        }
 
-          // Strings have a reference of data types
-          String name = "Azima";
-          String food = "sushi";
-          String email = "fake123@gmail.com";
-          String car = "Mustang";
-          String color = "red";
+        // Common issues
+        System.out.print("Enter your age: ");
+        int age2 = scanner.nextInt();
+        scanner.nextLine();
 
-          System.out.println("Hello "+ name);
-          System.out.println("You are " + age + " years old");
-          System.out.println("Your gpa is " + gpa);
-          System.out.println("Your average letter grade is: " + grade);
-          System.out.println("Your favorite is " + food);
-          System.out.println("Your email is " + email);
+        System.out.print("Enter your favorite color");
+        String color = scanner.nextLine();
 
-          System.out.println("Your choice is a " + color + " " + year +" " + car);
-          System.out.println("The price is " + currency + price);
+        System.out.println("You are " + age2 + " years old");
+        System.out.println("You like the color " + color);
 
-          if(forSale){
-              System.out.println("There is a " + car + " for sale");
-          }else {
-              System.out.println("The " + car + " is not for sale " );
-          }
 
+
+        scanner.close();
+        // if u dont close it can lead to the unexpected behaviour
     }
 }
