6b073f3dd355ad488fdff79943489263f48f931e
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-07 15:48:03 +0100

    for each loop

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,36 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // for loop = execute some code a certain amount of times
 
-//      // Number guessing game
-        Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        int guess;
-        int attempts =0 ;
-        int max = 100;
-        int min = 1;
-        int randomNumber = random.nextInt(min,max+1);
+        System.out.print("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
 
-        System.out.println("Number Guessing Game");
-        System.out.println("Guess a number between " + min + "-" + max+ ": ");
-
-        do{
-            System.out.print("Enter a guess: ");
-            guess = scanner.nextInt();
-            attempts++ ;
-            if(guess < randomNumber){
-                System.out.println("TOO LOW, try again");
-            }
-            else if(guess >randomNumber){
-                System.out.println("TOO HIGH, try again");
-            }
-            else {
-                System.out.println("Correct! The number was " + randomNumber);
-                System.out.println("# of attempts: " + attempts );
-            }
-        }while(guess != randomNumber);
+        for(int i = 1; i<=max ; i++){
+            System.out.println(i);
+        }
 
         scanner.close();
+
+//        for(int i = 0; i< 10 ; i++){
+//            System.out.println("pizza");
+//            System.out.println(i);
+//        }
+//        for(int i = 1 ; i<= 10 ; i++){
+//            System.out.println(i);
+//        }
+//        for(int i= 10; i>0; i--){
+//            System.out.println(i);
+//        }
+//        for(int i =0; i<=10; i+=2){
+//            System.out.println(i);
+//        }
+
+//        for(int i = 10; i>=0; i*=2 ){
+//            System.out.println(i);
+//        }
     }
 }
