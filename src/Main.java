30b4e36b1aacade56f23cf75da95a13d40eb0aa8
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-07 15:58:15 +0100

    for each loop Practice

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // for loop = execute some code a certain amount of times
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many seconds to countdown from?:  ");
+        int start = scanner.nextInt();
 
-        System.out.print("Enter how many times you want to loop: ");
-        int max = scanner.nextInt();
-
-        for(int i = 1; i<=max ; i++){
+        for(int i = start; i>0 ; i--){
             System.out.println(i);
+            Thread.sleep(1000);
         }
-
-        scanner.close();
-
-//        for(int i = 0; i< 10 ; i++){
-//            System.out.println("pizza");
-//            System.out.println(i);
-//        }
-//        for(int i = 1 ; i<= 10 ; i++){
-//            System.out.println(i);
-//        }
-//        for(int i= 10; i>0; i--){
-//            System.out.println(i);
-//        }
-//        for(int i =0; i<=10; i+=2){
-//            System.out.println(i);
-//        }
-
-//        for(int i = 10; i>=0; i*=2 ){
-//            System.out.println(i);
-//        }
+        System.out.println("Happy new Year!");
+       scanner.close();
     }
 }
