1680cd9dd688ce0d005be227cb611fa585158363
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-07 14:22:40 +0100

    do while loop

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,28 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-//        String name = "";
-//        if(name.isEmpty()){
-//            System.out.print("Enter your name: ");
-//            name = scanner.nextLine();
-//        }
-
-//        while(name.isEmpty()){
-//            System.out.print("Enter your name: ");
-//            name = scanner.nextLine();
-//        }
+//        int age =0;
 //
-//        System.out.println("Hello "+ name);
-
-        // infinite loop
-
-//         String response ="";
-//        while(!response.equals("Q")){
-//            System.out.println("You are playing a game");
-//            System.out.print("Press Q to quit: ");
-//            response = scanner.next().toUpperCase();
+//        do{
+//            System.out.println("Your age cannot be negative");
+//            System.out.print("Enter your age: ");
+//            age = scanner.nextInt();
 //
+//        }while(age<0); // do while runs ur code at least once
+//
+//        System.out.println("You are " + age + " years old");
+
+        int number = 0 ;
+//        while(number<1 || number > 10){
+//            System.out.print("Enter a number between 1-10: ");
+//            number = scanner.nextInt();
 //        }
-//        System.out.println("You have quit the game");
+        do{
+           System.out.print("Enter a number between 1-10: ");
+            number = scanner.nextInt();
+        }while(number<1 || number > 10);
 
-        int age = 0;
-
-        System.out.print("Enter your age: ");
-        age = scanner.nextInt();
-        while(age < 0 ) { // while loop till it will false
-            System.out.println("Your age can't be negative");
-            System.out.print("Enter your age: ");
-            age = scanner.nextInt();
-        }
-
-        System.out.println("You are " + age + " years old ");
-
+        System.out.println("You picked " + number);
 
         scanner.close();
     }
