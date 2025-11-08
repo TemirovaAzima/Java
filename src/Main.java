dcd258f217ce86a138db700b2cff3600c6a11b13
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-08 17:21:16 +0100

    search elements in an Array

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,48 @@ public class Main {
     public static void main(String[] args) {
 
 //        Scanner scanner = new Scanner(System.in);
-//        String[] foods = new String[4];
-//        String[] foods;
-//        int size;
 //
-//        System.out.print("What # of food do you want: ");
-//        size = scanner.nextInt();
-//        scanner.nextLine();
+//     int[] numbers = {1,2,3,4,5,9,2,7};
+//        String[] fruits = {"apple","orange","banana"};
+//        boolean isFound = false;
+//        String target;
 //
-//        foods = new String[size];
+//        System.out.print("Enter a fruit to search for: ");
+//        target = scanner.nextLine();
+//       int target =  2;
 //
-//        for(int i = 0 ; i < foods.length ; i++){
-//            System.out.print("Enter a food: ");
-//            foods[i] = scanner.nextLine();
+//        for(int i = 0; i< fruits.length; i++){
+//            if (fruits[i].equals(target)){
+//                System.out.println("Element found at index " + i);
+//                isFound = true;
+//                break;
+//            }
 //        }
-//        for(String food: foods){
-//            System.out.println(food);
+//
+//        if(!isFound){
+//            System.out.println("Element not found in the array");
 //        }
 //        scanner.close();
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] foods ;
-        int size;
+        String[] fruits = {"orange","apple", "pineapple","coconut"};
+        String target ;
+        boolean isFound = false;
 
-        System.out.print("What # of food do you want: ");
-        size = scanner.nextInt();
-        scanner.nextLine();
+        System.out.print("Enter a fruit to search for: ");
+        target = scanner.nextLine();
 
-        foods = new String[size];
-
-        for(int i = 0 ; i < foods.length; i++){
-            System.out.print("Enter a food you want: ");
-            foods[i] = scanner.nextLine();
+        for(int i = 0; i< fruits.length; i++){
+            if(fruits[i].equals(target)){
+                System.out.println("element found at index: " + i);
+                isFound = true;
+            }
         }
 
-        for(String food : foods){
-            System.out.println(food);
+        if(!isFound){
+            System.out.println("Element not found in the array");
         }
-        scanner.close();
+
     }
 }
