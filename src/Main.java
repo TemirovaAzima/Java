39c269324f0683632d8f3f3e3c3eada9cae492f3
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-08 18:40:17 +0100

    mini project with 2d arrays

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,17 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        char[][] telephone = {{'1','2','3'},
+                              {'4','5','6'},
+                              {'7','8','9'},
+                              {'*','0','#'}};
 
-        // 2D arrays
-   //        String[] fruits = {"apple","orange","banana"};
-//        String[] vegetables = {"potato","onion","carrot"};
-//        String[] meats = {"chicken","pork","beef","fish"};
-//
-//        String[][] groceries = {fruits, vegetables, meats};
-          String[][] groceries = {{"apple","orange","banana"},
-                                  {"potato","onion","carrot"},
-                                  {"chicken","pork","beef","fish"}  };
-
-          groceries[1][2] = "celery";
-//        for(String[] foods: groceries){
-//            for (String food : foods){
-//                System.out.print(food + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println(fruits.length);
-        for(int i = 0; i < groceries.length; i++){
-            for(int j = 0 ; j<groceries[i].length; j++){
-                System.out.print(groceries[i][j] + " ");
+        for(char[] row : telephone){
+            for(char number: row){
+                System.out.print(number + " ");
             }
             System.out.println();
         }
-
-
-
-
-
-
 
     }
 }
