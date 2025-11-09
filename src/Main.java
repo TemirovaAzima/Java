f608fdb93cfcff1309c2bb9f1b04935abbee537d
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-09 20:09:10 +0100

    Task 4 , 2 and 1 done from Test

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,118 @@
-
-public class Main {
-    public static void main(String[] args) {
-//        int x = 5;
-//        double y = 2.5;
-//       int  result = (int)( x + y);
-//        System.out.println(result);
-
-//        int x = 5;
-//        if(x > 0) System.out.println("x is positive");
 //
-//        for(int i = 0; i < 3 ; i++) System.out.println("hell");
+//public class Main {
+//    public static void main(String[] args) {
+////        int x = 5;
+////        double y = 2.5;
+////       int  result = (int)( x + y);
+////        System.out.println(result);
 //
-//        for(int i = 0 ; i <3 ; i++)
-//            System.out.println("H");
-//        System.out.println("W");
-
-//        int[] numbers = {1,2,3};
-//        System.out.println("Original length: " + numbers.length);
+////        int x = 5;
+////        if(x > 0) System.out.println("x is positive");
+////
+////        for(int i = 0; i < 3 ; i++) System.out.println("hell");
+////
+////        for(int i = 0 ; i <3 ; i++)
+////            System.out.println("H");
+////        System.out.println("W");
 //
-//        // numbers.length = 5 ;
+////        int[] numbers = {1,2,3};
+////        System.out.println("Original length: " + numbers.length);
+////
+////        // numbers.length = 5 ;
+////
+////        int[] biggerArray = new int[5];
+////
+////        for (int i = 0 ; i < numbers.length; i++){
+////            biggerArray[i] = numbers[i];
+////        }
+////        biggerArray[3] = 4;
+////        biggerArray[4] = 5;
 //
-//        int[] biggerArray = new int[5];
 //
-//        for (int i = 0 ; i < numbers.length; i++){
-//            biggerArray[i] = numbers[i];
-//        }
-//        biggerArray[3] = 4;
-//        biggerArray[4] = 5;
-
-
-//        System.out.println("New length: " + biggerArray.length);
-//        for (int num : biggerArray){
-//            System.out.print(num + " ");
-//        }
-
-//         int[] arr = {1,2,3};
-//         System.out.println("Before: " + Arrays.toString(arr));
+////        System.out.println("New length: " + biggerArray.length);
+////        for (int num : biggerArray){
+////            System.out.print(num + " ");
+////        }
 //
-//         arr = Arrays.copyOf(arr,5);
-//         arr[3] = 4 ;
-//         arr[4] = 5;
+////         int[] arr = {1,2,3};
+////         System.out.println("Before: " + Arrays.toString(arr));
+////
+////         arr = Arrays.copyOf(arr,5);
+////         arr[3] = 4 ;
+////         arr[4] = 5;
+////
+////        System.out.println("After: " + Arrays.toString(arr));
 //
-//        System.out.println("After: " + Arrays.toString(arr));
-
-//        int a = 3; // 4 bytes
-//        short l = 32000; // 2 bytes
-//        double b = 3;  // 8 bytes
-//        float c = 3.14f; // 4 bytes
-//        double d = 3.14159299999944;  // 8 bytes
-//        long e = 100000000000L;  // 8 bytes
-//        char f = 'a'; // 2 bytes
-//        System.out.println(a + " " + b + " " + c + " " + d  + " "+ e );
-
-//        char c = 10 ;
-//        short s = 20;
+////        int a = 3; // 4 bytes
+////        short l = 32000; // 2 bytes
+////        double b = 3;  // 8 bytes
+////        float c = 3.14f; // 4 bytes
+////        double d = 3.14159299999944;  // 8 bytes
+////        long e = 100000000000L;  // 8 bytes
+////        char f = 'a'; // 2 bytes
+////        System.out.println(a + " " + b + " " + c + " " + d  + " "+ e );
 //
-//        int result = c + s ;
-//        System.out.println( result);
-
-//        long l = 5L;
-//        float f = 2.5f;
-//        long result = (long) (l + f) ;
-//        System.out.println(result);
+////        char c = 10 ;
+////        short s = 20;
+////
+////        int result = c + s ;
+////        System.out.println( result);
 //
-//        byte a = 100;
-//        byte b = 50;
-//        int c = ( a + b) ;
-//        System.out.println(c);
+////        long l = 5L;
+////        float f = 2.5f;
+////        long result = (long) (l + f) ;
+////        System.out.println(result);
+////
+////        byte a = 100;
+////        byte b = 50;
+////        int c = ( a + b) ;
+////        System.out.println(c);
+//
+////        int a = 1000;
+////        long b = 2000L;
+////        long c = a + b ;
+////        System.out.println(c);
+////
+////        int a1 = 1000;
+////        int b1 = 2000;
+////        int c1 = a1 + b1;
+////        System.out.println(c1);
+//
+//    }
+//}
 
-        int a = 1000;
-        long b = 2000L;
-        long c = a + b ;
-        System.out.println(c);
-
-        int a1 = 1000;
-        int b1 = 2000;
-        int c1 = a1 + b1;
-        System.out.println(c1);
+void main(){
+//    int x = 5;
+//    int y ;
+//
+//    y = x;     // = assigning
+//    System.out.println(y);
+//
+//    if ( x == y ){  // == checking
+//        System.out.println("The x and y are equal");
+//    }
+    char stop = 'a' + 10;
+    String text = "";
+    for(char c = 'a'; c < stop; c+=2 ){
+        System.out.print(c - 'a');
+        System.out.print(' ');
+        text +=c;
     }
+    System.out.println();
+    for(int i = text.length()-1 ; i>=0; i -=1){
+        System.out.print(text.charAt(i));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
