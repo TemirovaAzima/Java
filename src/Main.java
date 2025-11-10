656fc84d66db7b2437bc5e47ca06791142f2a7e2
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-10 17:56:01 +0100

    PPJ 08 done

--- src/Main.java
+++ src/Main.java
@@ -1,187 +1,160 @@
-//
-//public class Main {
-//    public static void main(String[] args) {
-////        int x = 5;
-////        double y = 2.5;
-////       int  result = (int)( x + y);
-////        System.out.println(result);
-//
-////        int x = 5;
-////        if(x > 0) System.out.println("x is positive");
-////
-////        for(int i = 0; i < 3 ; i++) System.out.println("hell");
-////
-////        for(int i = 0 ; i <3 ; i++)
-////            System.out.println("H");
-////        System.out.println("W");
-//
-////        int[] numbers = {1,2,3};
-////        System.out.println("Original length: " + numbers.length);
-////
-////        // numbers.length = 5 ;
-////
-////        int[] biggerArray = new int[5];
-////
-////        for (int i = 0 ; i < numbers.length; i++){
-////            biggerArray[i] = numbers[i];
-////        }
-////        biggerArray[3] = 4;
-////        biggerArray[4] = 5;
-//
-//
-////        System.out.println("New length: " + biggerArray.length);
-////        for (int num : biggerArray){
-////            System.out.print(num + " ");
-////        }
-//
-////         int[] arr = {1,2,3};
-////         System.out.println("Before: " + Arrays.toString(arr));
-////
-////         arr = Arrays.copyOf(arr,5);
-////         arr[3] = 4 ;
-////         arr[4] = 5;
-////
-////        System.out.println("After: " + Arrays.toString(arr));
-//
-////        int a = 3; // 4 bytes
-////        short l = 32000; // 2 bytes
-////        double b = 3;  // 8 bytes
-////        float c = 3.14f; // 4 bytes
-////        double d = 3.14159299999944;  // 8 bytes
-////        long e = 100000000000L;  // 8 bytes
-////        char f = 'a'; // 2 bytes
-////        System.out.println(a + " " + b + " " + c + " " + d  + " "+ e );
-//
-////        char c = 10 ;
-////        short s = 20;
-////
-////        int result = c + s ;
-////        System.out.println( result);
-//
-////        long l = 5L;
-////        float f = 2.5f;
-////        long result = (long) (l + f) ;
-////        System.out.println(result);
-////
-////        byte a = 100;
-////        byte b = 50;
-////        int c = ( a + b) ;
-////        System.out.println(c);
-//
-////        int a = 1000;
-////        long b = 2000L;
-////        long c = a + b ;
-////        System.out.println(c);
-////
-////        int a1 = 1000;
-////        int b1 = 2000;
-////        int c1 = a1 + b1;
-////        System.out.println(c1);
-//
-//    }
-//}
-
 void main(){
-//    int x = 5;
-//    int y ;
+
+//    for(int i = 0; i< 10 ; i++){
+//        System.out.println(i);
+//    }
 //
-//    y = x;     // = assigning
-//    System.out.println(y);
+//    // exercise 2
+//    int wrt = 2;
+//    for(int i = 0; i< 10 ; i++){
+//        System.out.println(i * wrt);
+//    }
 //
-//    if ( x == y ){  // == checking
-//        System.out.println("The x and y are equal");
+//    // exercise 3
+//
+//    int num1;
+//    int num2;
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.print("Enter the first number: ");
+//    num1 = scanner.nextInt();
+//
+//    System.out.print("Enter the second number: ");
+//    num2 = scanner.nextInt();
+//
+//    if(num1 > 0 && num2 > 0){
+//        System.out.println(num1 - num2);
 //    }
-//    char stop = 'a' + 10;
-//    String text = "";
-//    for(char c = 'a'; c < stop; c+=2 ){
-//        System.out.print(c - 'a');
-//        System.out.print(' ');
-//        text +=c;
+//    else {
+//        System.out.println(num1 + num2);
 //    }
-//    System.out.println();
-//    for(int i = text.length()-1 ; i>=0; i -=1){
-//        System.out.print(text.charAt(i));
+//
+//    // exercise 4
+//    char ch ;
+//    System.out.print("Enter a character: ");
+//    ch = scanner.next().toLowerCase().charAt(0);
+//    ch = Character.toLowerCase(ch);
+
+//    switch (ch) {
+//        case 'a', 'e', 'i', 'o', 'u' -> System.out.print("it is a vowel");
+//        default -> {
+//        if (Character.isLetter(ch)) {
+//            System.out.println("it is consonant");
+//        } else {
+//            System.out.println("it is not a letter");
+//        }
 //    }
-//    boolean running = false;
-//    int number      = 1337;
+//    }
+
+    // exercise 5
+
+//    boolean running = false ;
+//    do {
+//        System.out.print("Do you like running (true/false): ");
+//        running = scanner.nextBoolean();
+//    }while(running);
+//
+//    while(running){
+//        System.out.println("You like running ");}
+//
+//        // next example
+//        int number = 1337;
+//        boolean isRunning = false;
+//        do{
+//            isRunning = number>0;
+//            System.out.println(number/10);
+//            number /=10;
+//        }while(isRunning);
+
+//    int a = 0;
+//    while(a>0){
+//        System.out.println("While loop executed");
+//    }
+
+//    do{
+//        System.out.println("Do-while loop executed at least once");
+//    }while(a>0);
+
+//    int checkWhile =0;
+//    while(true){
+//        System.out.println(checkWhile++);
+//        if(checkWhile ==10){
+//            break;
+//        }
+//    }
+
+//    do {
+//        System.out.println(checkWhile++);
+//        if (checkWhile ==10){
+//            break;
+//        }
+//    }while(true);
+
+    // exercise 6
+
+//    int a = 5;
+//    if(a<=0){
+//        System.out.println("Negative");
+//        a = a * -1;
+//        System.out.println("Absolute value is : " + a);
+//    }else{
+//        System.out.println("Positive");
+//        System.out.println("Absolute value is: "+ a);
+//    }
+//    if(a<=0){
+//        System.out.println("Negative");
+//        a = a * -1;
+//        System.out.println("Absolute value is : " + a);
+//    }
+//    if (a>=0){
+//        System.out.println("Positive");
+//        System.out.println("Absolute value is: "+ a);
+//    }
+
+    // exercise 7
+    Scanner scanner = new Scanner(System.in);
+//    int id ;
+//    System.out.print("Enter ur student ID: ");
+//    id = scanner.nextInt();
+//    int sum =0 ;
+//    for(int i =0; i <= id; i++){
+//        sum +=i;
+//    }
+//    System.out.println(sum);
+
+    // exercise 8
+//
+//    int userInput ;
+//    int numberCount = 0;
+//    int sumOfNum = 0;
+//
 //
 //    do {
-//        running = number > 0;
-//        System.out.println(number / 10);
-//        number /= 10;
-//    } while (running);
-//
-//    System.out.println(017 == 17); // this 0 is octal u cant ignore the zero if u convert to decimal
-    // it will become 15 and 15 = 17 no so it is false
-//    System.out.println(017);
-//    System.out.println(0b10101); // it is not -11 cause int is 32 bits so even though there is 1 in
-    // the first significant term , java convertes to decimal and it will become int not short or byte
-    // this is just how java works u know so before this 1 there is 27 zeros so i
-    // the first significant does not consider as a negative number keep in mind
-
-//    System.out.println(0xFF);
-
-//    byte b = 0b10101;
-//    short s = 0b10101;
-//    int i = 0b10101;
-//    System.out.println(b + s + i);
-//    System.out.println(0b10101L);
-
-    // SO 0 IS OCTAL , OX IS HEXADECIMAL , 0b IS BINARY
-    // 0x MEANS BASE 16
-//
-//    String inHex = "2FA3";
-//
-//    System.out.println(0x2FA3);
-////    int value = (int) (2 * Math.pow(16,3) + 15 * Math.pow(16,2) + 10* 16 + 3 * Math.pow(16,0));
-//    int value = 2 * 16*16*16 + 15 * 16*16 + 10 * 16 + 3;
-//
-//    System.out.println(value);
-
-//    int [] arr = new int[15];
-//
-//    for(int i = 0 ; i < arr.length; i++){
-//        arr[i] = (int) (Math.random() * 20+1);
-//        System.out.println(arr[i] + " ");
-//
-//        if(arr[i] % 2 ==0 && arr[i] > 10){
-//            arr  = new int[i];
-////            System.out.println(arr[i]);
+//        System.out.print("Enter a number: ");
+//        userInput = scanner.nextInt();
+//        if(userInput != 0){
+//            numberCount++;
+//            sumOfNum += userInput;
 //        }
-//    }
-//
-//    for(int i = arr.length-1; i > 0 ; i--){
-//        System.out.print(arr[i] + " ");
-//    }
+//    }while(userInput !=0);
+//    System.out.println("The total count of numbers: "+numberCount);
+//    System.out.println("The sum : " + sumOfNum);
 
-//    int[] arr = new int[15];
-
-//// Fill the array with random numbers 1–20
-//    for (int i = 0; i < arr.length; i++) {
-//        arr[i] = (int) (Math.random() * 20 + 1);
-//        System.out.print(arr[i] + " ");
-//    }
+//    char ch = 'K';
+//    System.out.println(ch);
 //
-//    System.out.println(); // move to a new line
-//
-//// Print even numbers > 10 in reverse order
-//    for (int i = arr.length - 1; i >= 0; i--) {
-//        if (arr[i] % 2 == 0 && arr[i] > 10) {
-//            System.out.print(arr[i] + " ");
+//    for(int i = 75 ; i <150 ; i++){
+//        for(char ch = 'K' ; ch < '–'; ch++){
+//            System.out.print(i);
+//            System.out.println(ch);
 //        }
+//    } // is wrong
+//
+//    for(int i = 75 ; i <= 150; i++){
+//        char ch = (char) i;
+//        System.out.println(ch + " " + i);
 //    }
 
-    int[] arr = new int[15];
 
-    for(int i = 0 ; i < arr.length; i++){
-        arr[i] =  (int)(Math.random() * 20 + 1);
-        System.out.print(arr[i] + " ");
-    }
 
-    System.out.println();
-    for(int i = arr.length -1; i >0 ; i--){
-        if(arr[i] % 2 ==0 && arr[i] > 10){
-            System.out.print(arr[i]+ " ");
-        }
-    }
+
 }
