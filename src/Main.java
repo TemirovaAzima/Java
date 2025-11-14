a88a98755456cc067a92f5a851f8fab138877c25
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-14 02:02:29 +0100

    PPJ012 - task 1 completed

--- src/Main.java
+++ src/Main.java
@@ -1040,15 +1040,32 @@ void main() {
 //        System.out.print(text.charAt(i));
 //    }
 
-    int[][] arr = {{1,2,3,4,5},
-                  {5,4,3,2,1}};
-    int sum = 0;
-    int sum2 = 0;
+//    int[][] arr = {{1,2,3,4,5},
+//                  {5,4,3,2,1}};
+//    int sum = 0;
+//    int sum2 = 0;
+//
+//    sum = arr[0][arr[0].length -1] + arr[arr.length-1][0];
+//    sum2 = arr[arr.length-1][0];
+//    System.out.println(sum);
+//    System.out.println(sum2);
 
-    sum = arr[0][arr[0].length -1] + arr[arr.length-1][0];
-    sum2 = arr[arr.length-1][0];
+    int[][] arr = new int[10][10];
+
+    int sum =0;
+    for(int i = 0; i < arr.length;i++){
+        for(int j = 0; j < arr[i].length; j++){
+            arr[i][j] = (int) (Math.random()*7+1);
+            sum += arr[arr.length-1][j];
+        }
+    }
+//    System.out.println(Arrays.toString(arr));
+
+    for(int i = 0; i < arr.length;i++){
+        for(int j =0; j < arr[i].length;j++){
+            System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+    }
     System.out.println(sum);
-    System.out.println(sum2);
-
-
 }
