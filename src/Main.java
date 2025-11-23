5e8baec0fec3b8c337f6040327db6a7c841432a3
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-23 16:26:38 +0100

    Recurve functions countUp

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 
 void main() {
-    for(int num = 0; num <=12 ; ++num){
-        System.out.println("Factorial of "+ num +
-                           " is " + RecFun.fact(num));
-    }
+//    for(int num = 0; num <=20 ; ++num){
+//        System.out.println("Factorial of "+ num +
+//                           " is " + RecFun.fact(num));
+//    }
+    int num = 4;
+    int result = countUp(num);
+//    System.out.println(result);
+
 
 }
-public class RecFun{
-    static int fact(int n){
-        if(n < 0) throw new IllegalArgumentException();
-        if(n<=1) return 1;
-        return n* fact(n-1);
-    }
+
+static int countUp(int n ){
+    if(n == 0){
+        return 0;
+    };
+     int result =  countUp(n-1) + 1;
+    System.out.println(result);
+     return result ;
+
 }
+//public class RecFun{
+//    static int fact(int n){
+//        if(n < 0) throw new IllegalArgumentException();
+//        if(n<=1) return 1;
+//        return n* fact(n-1);
+//    }
+//}
