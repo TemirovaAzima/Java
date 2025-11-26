33c03aaa44bb12ddd7aa2250a45f99886e25601c
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-26 19:09:18 +0100

    IsPalindrome() recursive function not mine

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
-
 void main() {
     String pal = "ABCDDCB";
     System.out.println(isPalindrome(pal,0,pal.length()-1));
 }
+// chatgpt version
 static boolean isPalindrome(String word,int i,int j){
     if(i>=j){
-        return word.charAt(i) == word.charAt(j);
+        return true;
     }
-    return word.charAt(i) == word.charAt(j) ? isPalindrome(word,i+1,j-1): false;
+    if(word.charAt(i) != word.charAt(j)){
+        return false;
+    }
+    return isPalindrome(word,i+1,j-1);
 }
+
+
+//static boolean isPalindrome(String word,int i,int j){
+//    if(i>=j){
+//        return word.charAt(i) == word.charAt(j);
+//    }
+//    return word.charAt(i) == word.charAt(j) ? isPalindrome(word,i+1,j-1): false;
+//}
 //static boolean palindrome(String word,int i,int j){
 //    if((word.length()-1) / 2== i){
 //        return word.charAt(i) == word.charAt(j);
