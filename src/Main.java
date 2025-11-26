235dd226e3fc16bd86733e36034323164527ace8
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-26 20:45:33 +0100

    countOccurrence() using recursive functions

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,11 @@
 void main(String[] args) {
-
-    int[] arr = {5,1,8,2,203,88,899,3};
-    // loop version findMax()
-    int maxNum = arr[0];
-    for(int i = 0; i < arr.length ; i++ ){
-        if(arr[i] > maxNum) {
-            maxNum = arr[i];
-        }
-    }
-    System.out.println(maxNum);
-//    System.out.println(findMax(arr,0));
+    int[] arr = {1,2,3,2,2};
+    System.out.println(countOccurrence(arr,2,0));
 }
-
-//static int findMax(int[] arr,int i){
-//    if(arr.length-1 == i){
-//        return arr[i];
-//    }
-//    int nextMax = findMax(arr,i+1); // we dont repeat here the findMax twice
-//    return arr[i] > nextMax ? arr[i] : nextMax;
-//}
-//static int findMax(int[] arr,int i){
-//    if(arr.length-1 == i){
-//        return arr[i];
-//    }
-//    return arr[i] > findMax(arr,i+1) ? arr[i] : findMax(arr, i+1);
-//}
+static int countOccurrence(int[] arr,int num , int i ){
+    if(arr.length == i ){
+        return 0;
+    }
+    return (arr[i] == num ? 1: 0) + countOccurrence(arr, num,i+1);
+}
 
