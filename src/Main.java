a617feee6a25277bd86558a423f4fc58f30a346c
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-26 20:06:45 +0100

    findMax() using Loop

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 void main(String[] args) {
 
     int[] arr = {5,1,8,2,203,88,899,3};
-    System.out.println(findMax(arr,0));
-}
-static int findMax(int[] arr,int i){
-    if(arr.length-1 == i){
-        return arr[i];
+    // loop version findMax()
+    int maxNum = arr[0];
+    for(int i = 0; i < arr.length ; i++ ){
+        if(arr[i] > maxNum) {
+            maxNum = arr[i];
+        }
     }
-    int nextMax = findMax(arr,i+1); // we dont repeat here the findMax twice
-    return arr[i] > nextMax ? arr[i] : nextMax;
+    System.out.println(maxNum);
+//    System.out.println(findMax(arr,0));
 }
+
+//static int findMax(int[] arr,int i){
+//    if(arr.length-1 == i){
+//        return arr[i];
+//    }
+//    int nextMax = findMax(arr,i+1); // we dont repeat here the findMax twice
+//    return arr[i] > nextMax ? arr[i] : nextMax;
+//}
 //static int findMax(int[] arr,int i){
 //    if(arr.length-1 == i){
 //        return arr[i];
