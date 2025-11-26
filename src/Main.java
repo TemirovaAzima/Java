1f33eb3bef8f278c888971c1cf03cd66667e7a91
TemirovaAzima <temirovaazima9@gmail.com>
2025-11-26 20:55:26 +0100

    countOccurrence() using LOOP

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 void main(String[] args) {
     int[] arr = {1,2,3,2,2};
+    int value = 2;
+    int count = 0;
+    // loop version countOccurrence
+    for(int i = 0; i< arr.length; i++){
+        if(arr[i] == value){
+            count++;
+        }
+    }
+    System.out.println(count);
+    // enhanced loop
+    for(int num : arr){
+        if(num == value){
+            count++;
+        }
+    }
     System.out.println(countOccurrence(arr,2,0));
+    System.out.println(countsOccurrence(arr,value));
+}
+// or even loop inside the function
+static int countsOccurrence(int[] arr,int value){
+    int count = 0;
+    for(int num : arr){
+        if(num == value){
+            count++;
+        }
+    }
+    return count;
 }
 static int countOccurrence(int[] arr,int num , int i ){
     if(arr.length == i ){
